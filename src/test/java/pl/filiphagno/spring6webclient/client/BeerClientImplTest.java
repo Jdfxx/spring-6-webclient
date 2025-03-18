63f4d35d37d2a4854a944d99824b8b3456e86f70
Filip <fhagno@yahoo.com>
2025-03-18 09:55:12 +0100

    Add awaitality

--- src/test/java/pl/filiphagno/spring6webclient/client/BeerClientImplTest.java
+++ src/test/java/pl/filiphagno/spring6webclient/client/BeerClientImplTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
+
 @SpringBootTest
 class BeerClientImplTest {
 
@@ -12,6 +16,26 @@ class BeerClientImplTest {
 
     @Test
     void listBeers() {
-        beerClient.listBeers().subscribe(System.out::println);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        beerClient.listBeers().subscribe(response -> {
+                    System.out.println(response);
+                    atomicBoolean.set(true);
+                }
+        );
+
+        await().untilTrue(atomicBoolean);
+    }
+
+    @Test
+    void listBeersMap() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        beerClient.listBeerMap().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
+
     }
 }
